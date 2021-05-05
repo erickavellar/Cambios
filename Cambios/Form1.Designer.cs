c68c90aba29ac078b9199363f1d393444cda04b9
erickavellar <43542801+erickavellar@users.noreply.github.com>
2021-05-05 12:18:11 +0100

    Continue project

--- Cambios/Form1.Designer.cs
+++ Cambios/Form1.Designer.cs
@@ -29,13 +29,141 @@ namespace Cambios
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ComboBoxOrigem = new System.Windows.Forms.ComboBox();
+            this.ComboBoxDestino = new System.Windows.Forms.ComboBox();
+            this.ButtonConverter = new System.Windows.Forms.Button();
+            this.LabelResultado = new System.Windows.Forms.Label();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(38, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor:";
+            // 
+            // TextBoxValor
+            // 
+            this.TextBoxValor.Location = new System.Drawing.Point(112, 69);
+            this.TextBoxValor.Name = "TextBoxValor";
+            this.TextBoxValor.Size = new System.Drawing.Size(572, 23);
+            this.TextBoxValor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(38, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Moeda de origem:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(38, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Moeda de destino:";
+            // 
+            // ComboBoxOrigem
+            // 
+            this.ComboBoxOrigem.FormattingEnabled = true;
+            this.ComboBoxOrigem.Location = new System.Drawing.Point(194, 132);
+            this.ComboBoxOrigem.Name = "ComboBoxOrigem";
+            this.ComboBoxOrigem.Size = new System.Drawing.Size(490, 23);
+            this.ComboBoxOrigem.TabIndex = 4;
+            // 
+            // ComboBoxDestino
+            // 
+            this.ComboBoxDestino.FormattingEnabled = true;
+            this.ComboBoxDestino.Location = new System.Drawing.Point(194, 205);
+            this.ComboBoxDestino.Name = "ComboBoxDestino";
+            this.ComboBoxDestino.Size = new System.Drawing.Size(490, 23);
+            this.ComboBoxDestino.TabIndex = 5;
+            // 
+            // ButtonConverter
+            // 
+            this.ButtonConverter.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonConverter.Location = new System.Drawing.Point(714, 69);
+            this.ButtonConverter.Name = "ButtonConverter";
+            this.ButtonConverter.Size = new System.Drawing.Size(131, 62);
+            this.ButtonConverter.TabIndex = 6;
+            this.ButtonConverter.Text = "Converter";
+            this.ButtonConverter.UseVisualStyleBackColor = true;
+            // 
+            // LabelResultado
+            // 
+            this.LabelResultado.AutoSize = true;
+            this.LabelResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelResultado.Location = new System.Drawing.Point(235, 276);
+            this.LabelResultado.Name = "LabelResultado";
+            this.LabelResultado.Size = new System.Drawing.Size(356, 21);
+            this.LabelResultado.TabIndex = 7;
+            this.LabelResultado.Text = "Escolha um valor, moeda de origem e destino";
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Location = new System.Drawing.Point(38, 387);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(38, 15);
+            this.LabelStatus.TabIndex = 8;
+            this.LabelStatus.Text = "status";
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(637, 375);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(208, 23);
+            this.ProgressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(876, 450);
+            this.Controls.Add(this.ProgressBar1);
+            this.Controls.Add(this.LabelStatus);
+            this.Controls.Add(this.LabelResultado);
+            this.Controls.Add(this.ButtonConverter);
+            this.Controls.Add(this.ComboBoxDestino);
+            this.Controls.Add(this.ComboBoxOrigem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxValor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Câmbios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ComboBoxOrigem;
+        private System.Windows.Forms.ComboBox ComboBoxDestino;
+        private System.Windows.Forms.Button ButtonConverter;
+        private System.Windows.Forms.Label LabelResultado;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.ProgressBar ProgressBar1;
     }
 }
 
--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,21 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Cambios
+﻿namespace Cambios
 {
+    using Modelos;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Windows.Forms;
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            LoadRates();
         }
 
+        private async void LoadRates()
+        {
+            //bool load;
+
+            ProgressBar1.Value = 0;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://cambios.somee.com");
+
+            var response = await client.GetAsync("/api/rates");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            ComboBoxOrigem.DataSource = rates;
+            ComboBoxOrigem.DisplayMember = "Name";
+
+            //Corrige bug da microsoft
+            ComboBoxDestino.BindingContext = new BindingContext();
+
+            ComboBoxDestino.DataSource = rates;
+            ComboBoxDestino.DisplayMember = "Name";
+
+            ProgressBar1.Value = 100;
+
+        }
     }
 }
